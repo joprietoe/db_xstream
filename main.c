46191ba909c545fd1e2cf20385052e06e8b325e4
joprietoe <joprietoe@gmail.com>
2016-08-17 16:12:56 -0300

    update options and queries

--- main.c
+++ main.c
@@ -15,260 +15,194 @@
 #include "generator.h"
 //#include "queries.h"
 
-/*
- -o  i import from file
- -o  r random
- -o  B deBruijn graph
-*/
-
 struct _globalArgs {
-	char* vertex_Filename;				/* -o = i -v option */
-	char *edge_Filename;				/* -o = i -e option */
-	char *database_Filename;	/* -o = i -b option */
-    float dens;   /* -o = r and -d */
-    //const int nodes;   /* -o = r and -n */
-    int m;  /* -o = B and -m */ //number of symbols
-    int n; /* -o = B  and -n length of string  -o = r and -n number of nodes*/
-    char *op; /* -o */
+  char *vertex_Filename;   /* -o = i -v option */
+  char *edge_Filename;     /* -o = i -e option */
+  char *database_Filename; /* -o = i -b option */
+  float dens;              /* -o = r and -d */
+  // const int nodes;   /* -o = r and -n */
+  int m; /* -o = B and -m */ // number of symbols
+  int n;    /* -o = B  and -n length of string  -o = r and -n number of nodes*/
+  char *op; /* -o */
+  int64_t vinit;
+  int64_t low_id;
 
 } globalArgs;
 
- //m - number of symbols 
-    //n - length of strings
 
-void convert_document( void )
-{
-	printf("Vertex %s\n",globalArgs.vertex_Filename);
-	printf("Edge %s\n",globalArgs.edge_Filename);
-	printf("Data %s\n",globalArgs.database_Filename);
-    printf("Data %f\n",globalArgs.dens);
-    printf("Data %d\n",globalArgs.m);
-    printf("Data %d\n",globalArgs.n);
-    printf("Data %s\n",globalArgs.op);
-    
+void show_init_data(void) {
+  printf("Vertex %s\n", globalArgs.vertex_Filename);
+  printf("Edge %s\n", globalArgs.edge_Filename);
+  printf("Data %s\n", globalArgs.database_Filename);
+  printf("Data %f\n", globalArgs.dens);
+  printf("Data %d\n", globalArgs.m);
+  printf("Data %d\n", globalArgs.n);
+  printf("Data %s\n", globalArgs.op);
 }
 
-static const char *optString = "o:v:e:d:n:m:b:";
+static const char *optString = "o:v:e:d:n:m:b:i:l:";
 
+pair *create_intervals(int64_t num, int n_int) {
+  pair *temp;
+  temp = malloc(n_int * sizeof(pair));
+  uint64_t pass, begin =  globalArgs.low_id;
+  pass = num / (int64_t)n_int;
+  int i; 
 
-pair * create_intervals(int64_t num, int n_int){
-		pair *temp;
-		temp = malloc(n_int * sizeof(pair));
-		uint64_t pass;
-        pass = num/(int64_t)n_int;
-		int i, begin = 1;
-		
-		for(i = 0; i < n_int; i++){
-				
-				pair t;
-				if(i < n_int - 1){
-						t.a = begin;
-						t.b = t.a + pass - 1;
-						begin += pass;
-					}
-				else{
-					 
-					t.a = begin;
-					t.b = num;
-					
-					}	
-				temp[i] = t;
-			}
-			
-			return temp;
-	}
+  for (i = 0; i < n_int; i++) {
 
-void empty_hash(Vertex **vertices){
+    pair t;
+    if (i < n_int - 1) {
+      t.a = begin;
+      t.b = t.a + pass - 1;
+      begin += pass;
+    } else {
 
-    Vertex *v, *tmp;
-    HASH_ITER(hh, *vertices, v, tmp) {
-        HASH_DEL(*vertices, v);
-        free(v);
+      t.a = begin;
+      t.b = num;
     }
+    temp[i] = t;
+  }
+ 
+  return temp;
 }
 
-void display_usage( void ){
-	printf( "-v - vertex file name\n" );
-    printf( "-e - edge file name\n" );
-    printf( "-b - database file name\n" );
-	/* ... */
-	exit( EXIT_FAILURE );
+void empty_hash(Vertex **vertices) {
+
+  Vertex *v, *tmp;
+  HASH_ITER(hh, *vertices, v, tmp) {
+    HASH_DEL(*vertices, v);
+    free(v);
+  }
 }
 
+void display_usage(void) {
+  printf("-v - vertex file name\n");
+  printf("-e - edge file name\n");
+  printf("-b - database file name\n");
+  /* ... */
+  exit(EXIT_FAILURE);
+}
+
+
+int64_t S64(const char *s) {
+  int64_t i;
+  char c ;
+  int scanned = sscanf(s, "%" SCNd64 "%c", &i, &c);
+  if (scanned == 1) return i;
+  if (scanned > 1) {
+    // TBD about extra data found
+    return i;
+    }
+  // TBD failed to scan;  
+  return -1;  
+}
 
 
 int main(int argc, char** argv) {
 
-    clock_t begin, end;
-    double time_spent = 0.0;
-    //FILE* fd = NULL;
-    FILE* fd = NULL;
-    time_t start_t, end_t;
-	double diff_t;
-    database db = NULL;
-    //globalArgs g_args;
-    
-    int opt = 0;
-	
-	/* Initialize globalArgs before we get to work. */
-	 globalArgs.vertex_Filename = NULL;		/* false */
-	 globalArgs.edge_Filename = NULL;
-	 globalArgs.database_Filename = NULL;
-     globalArgs.dens = 0.0;   /* -o = r and -d */
-     globalArgs.m = 0;  /* -o = B and -m */ //number of symbols
-     globalArgs.n = 0; /* -o = B  and -n length of string  -o = r and -n number of nodes*/
-     globalArgs.op = NULL;
-	
-	
-	/* Process the arguments with getopt(), then 
-	 * populate globalArgs. 
-	 */
-	//opt = getopt( argc, argv, optString );
-	while( (opt = getopt (argc, argv, optString)) != -1 ) {
-		switch( opt ) {
-			case 'o':
-                 globalArgs.op = strdup(optarg);
-                break;
-            case 'd':
-                 globalArgs.dens = atof(optarg);
-                break;
-            case 'm':
-                 globalArgs.m = atoi(optarg);
-                break;
-            case 'n':
-                 globalArgs.n = atoi(optarg);
-                break;            
-            case 'v':
-				 globalArgs.vertex_Filename = strdup(optarg);	/* true */
-				break;
-			case 'e':
-				 globalArgs.edge_Filename = strdup(optarg);
-				break;
-				
-			case 'b':
-				/* This generates an "assignment from
-				 * incompatible pointer type" warning that
-				 * you can safely ignore.
-				 */
-				 globalArgs.database_Filename = strdup(optarg);
-				break;
-			
-				
-			case 'h':	/* fall-through is intentional */
-			case '?':
-				display_usage();
-				break;
-				
-			default:
-				/* You won't actually get here. */
-				break;
-		}
-		
-		//opt = getopt( argc, argv, optString );
-	}
    
-
-    /********** INIT ****************/
-   convert_document();
-   create_database(&db,  "../grafo.db");
-    if (db == NULL) {
-        printf("Could not open database.");
-        return 1;
-    }
-
-    sql_stmt(db, "PRAGMA synchronous=OFF", NULL, NULL);
-    sql_stmt(db, "PRAGMA temp_store = MEMORY", NULL, NULL);
-    sql_stmt(db, "PRAGMA JOURNAL_MODE=OFF", NULL, NULL);
-   // sql_stmt(db, "PRAGMA LOCKING_MODE=EXCLUSIVE", NULL, NULL);
-    sql_stmt(db, "PRAGMA cache_size=10000", NULL, NULL);
-    sql_stmt(db, "PRAGMA mmap_size=268435456", NULL, NULL);
+  sql_stmt(db, "PRAGMA synchronous=OFF", NULL, NULL);
+  sql_stmt(db, "PRAGMA temp_store = MEMORY", NULL, NULL);
+  sql_stmt(db, "PRAGMA JOURNAL_MODE=OFF", NULL, NULL);
+  // sql_stmt(db, "PRAGMA LOCKING_MODE=EXCLUSIVE", NULL, NULL);
+  sql_stmt(db, "PRAGMA cache_size=5000", NULL, NULL);
+  sql_stmt(db, "PRAGMA main.cache_size=10000", NULL, NULL);
+  sql_stmt(db, "PRAGMA mmap_size=268435456", NULL, NULL);
+  //sql_stmt(db, "PRAGMA main.journal_mode=WAL;", NULL, NULL);
 
 
-   // create table if not exists edge (source integer, target integer, cost integer,  primary key (source,target))
-    /************ CREATE TABLES *********/
-    char *sql_create_edge = "create table if not exists edge" 
-                            "(source integer, target integer, cost integer)" ;
+  // create table if not exists edge (source integer, target integer, cost
+  // integer,  primary key (source,target))
+  /************ CREATE TABLES *********/
+  char *sql_create_edge = "create table if not exists edge"
+                          "(source integer, target integer, cost integer)  ";
 
-    char *sql_create_vertex = "create table if not exists vertex" 
-                            "(id integer , parent integer, phase integer)";
+  char *sql_create_vertex = "create table if not exists vertex"
+                            "(id integer , parent integer, phase integer)  ";
 
-   char *sql_create_update = "create table update_table" 
-                            "(id integer, parent integer not null, account integer)";
+  char *sql_create_update =
+      "create table update_table"
+      "(id integer, parent integer not null, account integer)";
 
-    sql_stmt(db, sql_create_edge, NULL, NULL);
-   
-    sql_stmt(db, sql_create_update, NULL, NULL);
-    sql_stmt(db, sql_create_vertex, NULL, NULL);
-    
-   
+  sql_stmt(db, sql_create_edge, NULL, NULL);
 
-    /********** END INIT ****************/
+  sql_stmt(db, sql_create_update, NULL, NULL);
+  sql_stmt(db, sql_create_vertex, NULL, NULL);
+
+  /********** END INIT ****************/
+
 
     /********** LOAD DATA ***************/
-   
-    int m, n;
-    int64_t nodes;
-    nodes = m = n = 0;
+  /* globalArgs.database_Filename = "/home/aluno/Julio-Work/vscode-workspace/db_xstream/grafo.db";
+  globalArgs.vertex_Filename = "/home/aluno/Julio-Work/Utils-db_stream/PaRMAT/Release/out_v.txt";
+  globalArgs.edge_Filename = "/home/aluno/Julio-Work/Utils-db_stream/PaRMAT/Release/out.txt";
+  globalArgs.vinit = 0;
+  globalArgs.op = "i";
+  globalArgs.low_id = 0;*/
 
-     begin = clock();
+  int64_t nodes;
+  int m, n;
+  nodes = m = n = 0;
 
-     //if(!strcmp(globalArgs.op, "r"))
-      //      gera_grafo(db,  globalArgs.n, globalArgs.m);
-     //else if(!strcmp(globalArgs.op, "B"))
-     //    igraph_de_bruijn(db, globalArgs.m,  globalArgs.n);
-     //else 
-       nodes = import_from_txt_file(db,  "/home/julio/vscode/db_xstream/out_v.txt",  "/home/julio/vscode/db_xstream/out.txt");
-   
-    // printf("Nodos y densidad: ");
-     //scanf("%d,%f",&nodes,&dens);
-     
-     //gera_grafo(db, nodes,dens);
-    
+  begin = clock();
+
+ if (!strcmp(globalArgs.op, "r"))
+    gera_grafo(db, globalArgs.n, globalArgs.m);
+  else if (!strcmp(globalArgs.op, "B"))
+    igraph_de_bruijn(db, globalArgs.m, globalArgs.n);
+  else
+    nodes = import_from_txt_file(db, globalArgs.vertex_Filename, globalArgs.edge_Filename);
+
+  // printf("Nodos y densidad: ");
+  // scanf("%d,%f",&nodes,&dens);
+
+  // gera_grafo(db, nodes,dens);
+
   /* printf("M,N: ");
    scanf("%d,%d", &m, &n);
 
    igraph_de_bruijn(db,m,n);*/
 
-   
-    
-   //sql_stmt(db,"create unique index unique_edge_index on edge (source,target)", NULL,NULL);
-   sql_stmt(db,"create unique index unique_vertex_index on vertex (id)", NULL,NULL);
-   //sql_stmt(db,"vacuum", NULL,NULL);
-  /* sql_stmt(db,"create view graph as select id_node, source, target, visit \ 
- 				from edge_table join node_table on edge_table.target = node_table.id_node");*/
+  // sql_stmt(db,"create unique index unique_edge_index on edge
+  // (source,target)", NULL,NULL);
+  //sql_stmt(db, "create unique index unique_vertex_index on vertex (id)", NULL, NULL);
+  // sql_stmt(db,"vacuum", NULL,NULL);
+  /* sql_stmt(db,"create view graph as select id_node, source, target, visit \
+                                from edge_table join node_table on
+     edge_table.target = node_table.id_node");*/
 
-    // insert_countries();
+  // insert_countries();
 
-      end = clock();
-    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    
-    printf("\nTime spent in graph creation %.3f",time_spent);
-    
-    fd = fopen("time.txt","w");
-    if(NULL == fd)
-    {
-        printf("\n fopen() Error!!!\n");
-        return 1;
-    }
-    
-    fprintf(fd, "Time spent in graph creation: %.3f\n", time_spent);
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("\nTime spent in graph creation %.3f", time_spent);
+
+  fd = fopen("time.txt", "w");
+  if (NULL == fd) {
+    printf("\n fopen() Error!!!\n");
+    return 1;
+  }
+
+  fprintf(fd, "Time spent in graph creation: %.3f\n", time_spent);
     
     /********************** CREATE INDEX ***************************/
-    //sql_stmt(db,"create unique index unique_edge_index on edge (source,target)", NULL, NULL);
-   // sql_stmt(db,"create unique index unique_vertex_index on vertex (id)", NULL, NULL);
-    
-    //printf("\nNumber of edges [%U]\n\n");
-    
-    //sql_stmt("select count(*) from edge", NULL, NULL);
-    
+  /********************** CREATE INDEX ***************************/
+  sql_stmt(db,"create index source_edge_index on edge (source)", NULL, NULL);
+  sql_stmt(db,"create unique index unique_vertex_index on vertex (id)", NULL, NULL);
 
-    //printf("\nNumber of nodes\n\n");
-   // select_stmt("select count(*) from node_table");
-    //select_stmt("select * from node_table");
+  // printf("\nNumber of edges [%U]\n\n");
 
-    //printf("\nVisit nodes\n\n");
-    //int a = isVisit(4);
-    //printf("Visit :%d \n",a);
+  // sql_stmt("select count(*) from edge", NULL, NULL);
+
+  // printf("\nNumber of nodes\n\n");
+  // select_stmt("select count(*) from node_table");
+  // select_stmt("select * from node_table");
+
+  // printf("\nVisit nodes\n\n");
+  // int a = isVisit(4);
+  // printf("Visit :%d \n",a);
     /********************* END LOAD DATA ************************/
 
     /********************* ALGORITHM  **************************/
@@ -294,7 +228,7 @@ int main(int argc, char** argv) {
 
 
     /*************** REAL STUFF ****************************/
-    init_alg(db,1);
+     init_alg(db, globalArgs.vinit);
 
     //bool gather_execution = true;
 
@@ -304,6 +238,7 @@ int main(int argc, char** argv) {
         phase += 1;
         //scatter
 
+        printf("DELETE FROM update_table\n ");
         sql_stmt(db,"DELETE FROM update_table", NULL, NULL);
         
         for(i = 0; i < number_of_intervals; i++){
@@ -314,22 +249,24 @@ int main(int argc, char** argv) {
              //unsigned int num_vertices;
              //num_vertices = HASH_COUNT(vertices);
             // printf("there are %u vertices interval \n", num_vertices);
-             
-              scatter_v2(db, intervals[i].a, intervals[i].b, phase, updates);
+
+            printf("begin scatter phase\n "); 
+            scatter_v2(db, intervals[i].a, intervals[i].b, phase, updates);
 
               if(utarray_len(updates) > 0)
                     global_execution = true;
 
+             printf("end scatter phase\n ");         
              //empty_hash(&vertices);
 
             
         }
 
         if(global_execution){
-            Update *p;
+            /*Update *p;
             for(p=(Update *)utarray_front(updates); p!=NULL; p=(Update *)utarray_next(updates,p)) {
                printf("id: [""%"PRId64"], parent: [""%"PRId64"], account [""%"PRId64"]\n",p->id, p->parent, p->account);
-            }
+            }*/
             apply_update(db,updates);
             gather(db);
         }
